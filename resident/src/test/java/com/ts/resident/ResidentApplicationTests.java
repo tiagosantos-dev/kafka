4563b281453a31ce0200853eed4a0777e23582ba
tiagosantos-dev <tiagoltj@hotmail.com>
2021-10-24 03:16:22 -0300

    Creating services

---
+++ resident/src/test/java/com/ts/resident/ResidentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.resident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ visitor/src/test/java/com/ts/visitor/VisitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.visitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
